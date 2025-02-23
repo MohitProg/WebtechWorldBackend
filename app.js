74fa293bc98b93779544650ba94067aa83a71e97
MohitProg <mohitjj999@gmail.com>
2025-02-23 17:08:35 +0530

    adding some changes

--- app.js
+++ app.js
@@ -21,7 +21,9 @@ app.use("/api/v1/blog",blogroute)
 app.use("/api/v1/user/project",projectroute)
 app.use("/api/v1/user/blog/comment",commentRoute)
 
-
+app.get("/",(req,res)=>{
+    return res.send("Website is running ")
+})
 
 
 
