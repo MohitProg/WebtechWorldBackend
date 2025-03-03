78138b7f1a6d6974d20e9a68fea9bcbf765edf6f
MohitProg <mohitjj999@gmail.com>
2025-03-03 21:31:45 +0530

    fix the issue

--- Middleware/multer.middleware.js
+++ Middleware/multer.middleware.js
@@ -1,27 +1,25 @@
 import multer from "multer";
-import path from "path"
-import fs from "fs"
+import path from "path";
+import fs from "fs";
 
-const storage=multer.diskStorage({
-    destination:function(req,file,cb){
-        const uploadPath = "public";
+var uploadPath = "public";
 
-        // Ensure the folder exists
-        if (!fs.existsSync(uploadPath)) {
-          fs.mkdirSync(uploadPath, { recursive: true });
-        }
+// Ensure the folder exists
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadPath);
+  },
 
-        cb(null,uploadPath)
-    },
-
-    filename:function(req,file,cb){
-        cb(null,file.originalname)
-    }
-})
-
-
- const upload=multer({
-    storage,
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
 
-export {upload};
+const upload = multer({
+  storage,
+});
+
+export { upload };
