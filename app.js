e6ea7b0adb94514ef222305191c973937e8b7c50
MohitProg <mohitjj999@gmail.com>
2025-03-03 19:00:29 +0530

    adding some changes

--- app.js
+++ app.js
@@ -14,10 +14,7 @@ app.use(express.static("public"));
 
 app.use(express.json())
 
-app.use(cors({
-    origin:["http://localhost:5173","https://webtechversion2.web.app"],
-    methods:["GET","POST","PUT","DELETE"]
-}));
+app.use(cors());
 
 
 
